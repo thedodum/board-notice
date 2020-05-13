911a00ec82d98d7f54156a3166b404599ad2047a
jeonyk <jeonyk@neoforth.com>
2020-05-13 13:53:56 +0900

    edit Validation

--- src/components/boardWrite.vue
+++ src/components/boardWrite.vue
@@ -3,43 +3,64 @@
     <div class="form-group">
       <h1>write</h1>
 
-      <form role="form">
-        <table class="table text-left">
-          <caption></caption>
-          <colgroup>
-            <col width="150">
-            <col>
-          </colgroup>
-          <tbody>
-          <tr>
-            <th>제목</th>
-            <td>
-              <validation-provider rules="minmax:3,8" v-slot="{ errors }">
-                <input type="text" class="form-control" placeholder="" v-model="title">
-                <span class="text-danger">{{ errors[0] }}</span>
-              </validation-provider>
-            </td>
-          </tr>
-          <tr>
-            <th>내용</th>
-            <td>
-              <textarea class="form-control" style="height: 200px;" v-model="desc"></textarea>
-            </td>
-          </tr>
-          <tr>
-            <th>첨부파일</th>
-            <td>
-              <input type="file" id="file-upload" class="form-control" v-on:change="fileSelect()" ref="listUploadFile">
-            </td>
-          </tr>
-          </tbody>
-        </table>
+      <ValidationObserver v-slot="{invalid}">
+        <form role="form" @submit.prevent="onSubmit">
+          <table class="table text-left">
+            <caption></caption>
+            <colgroup>
+              <col width="150">
+              <col>
+            </colgroup>
+            <tbody>
+            <tr>
+              <th>제목</th>
+              <td>
+                <ValidationProvider rules="required" v-slot="{ errors }">
+                  <input type="text" class="form-control" id="title_notice" v-model="title">
+                  <!--<span class="text-danger">{{ errors[0] }}</span>-->
+                  <ul class="" style="padding-left: 20px;">
+                    <li v-for="error in errors" class="text-danger">{{ error }}</li>
+                  </ul>
+                </ValidationProvider>
+              </td>
+            </tr>
+            <tr>
+              <th>이메일</th>
+              <td>
+                <ValidationProvider name="email" rules="required|email" :bails="false" v-slot="{ errors }">
+                  <input type="text" class="form-control" v-model="email">
+                  <span class="text-danger">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </td>
+            </tr>
+            <tr>
+              <th>내용</th>
+              <td>
+                <validation-provider rules="required" v-slot="{errors}">
+                  <textarea class="form-control" style="height: 200px;" name="desc_notice" v-model="desc"></textarea>
+                  <span class="text-danger">{{ errors[0] }}</span>
+                </validation-provider>
+              </td>
+            </tr>
+            <tr>
+              <th>첨부파일</th>
+              <td>
+                <validation-provider rules="required" v-slot="{errors}">
+                  <input type="file" id="file-upload" class="form-control" v-on:change="fileSelect()" ref="listUploadFile">
+                  <span class="text-danger">{{ errors[0] }}</span>
+                </validation-provider>
+              </td>
+            </tr>
+            </tbody>
+          </table>
 
-        <div class="clearfix">
-          <router-link to="/" tag="button" class="btn btn-default pull-left">list</router-link>
-          <button type="button" v-on:click="addList(title, desc)" class="btn btn btn-primary pull-right">write</button>
-        </div>
-      </form>
+          <div class="clearfix">
+            <router-link to="/" tag="button" class="btn btn-default pull-left">list</router-link>
+            <!--<button type="submit" v-on:click="addList(title, desc)" class="btn btn btn-primary pull-right">write</button>-->
+            <button type="submit" :disabled="invalid" class="btn btn btn-primary pull-right">write</button>
+          </div>
+        </form>
+      </ValidationObserver>
 
     </div>
 
@@ -47,14 +68,30 @@
 </template>
 
 <script>
-  import { ValidationProvider, extend } from 'vee-validate';
-  import { required } from 'vee-validate/dist/rules';
+  import { ValidationProvider, extend, ValidationObserver } from 'vee-validate';
+  // import { required, email } from 'vee-validate/dist/rules';
+  import * as rules from 'vee-validate/dist/rules';
+  import { messages } from 'vee-validate/dist/locale/ko.json';
+
+  Object.keys(rules).forEach(rule => {
+    extend(rule, {
+      ...rules[rule], // copies rule configuration
+      message: messages[rule] // assign message
+    });
+  });
+
+  /*for (let [rule, validation] of Object.entries(rules)) {
+    extend(rule, {
+      ...validation
+    });
+  }*/
 
   export default {
     name: 'boardWrite',
     data(){
       return {
         title: '',
+        email: '',
         desc: '',
         date: '',
         arr_list: [],
@@ -108,35 +145,75 @@
         }).catch((err) => {
           console.log(err);
         })
+      },
+      onSubmit(){
+        console.log('Form has been submitted!');
+        this.addList (this.title, this.desc);
       }
     },
     components: {
-      ValidationProvider
+      ValidationProvider,
+      ValidationObserver
     },
   }
+/*
+  extend('email', email);
 
-  extend('positive', value => {
-    return value >= 0;
+  extend('required', {
+    ...required,
+    message: '필수 입력'
   });
 
-  extend('odd', value => {
-    return value % 2 !== 0;
+  extend('required', {
+    validate (value) {
+      return {
+        required: true,
+        valid: ['', null, undefined].indexOf(value) === -1
+      };
+    },
+    computesRequired: true,
+    message: '필수 입력 사항!!! test'
+  });
+
+  extend('positive', value => {
+    if (value >= 0){
+      return true;
+    }
+
+    return '{_field_} 정수로 입력 가능';
+  });
+
+  extend('odd', {
+    validate: value => {
+      return value % 2 !== 0;
+    },
+    message: '홀수만 입력 가능'
   });
 
   extend('min', {
-    validate(value, args){
-      return value.length >= args.length;
+    validate(value, {length}){
+      return value.length >= length;
     },
-    params: ['length']
+    params: ['length'],
+    message: 'The {_field_} field must have at least {length} characters'
   });
 
   extend('minmax', {
-    validate(value, args){
+    validate(value, {min, max}){
       // const length = value.length;
-      return value.length >= args.min && value.length <= args.max;
+      return value.length >= min && value.length <= max;
     },
-    params: ['min', 'max']
+    params: ['min', 'max'],
+    message: 'The {_field_} field must have at least {min} characters and {max} characters at most',
+    /!*message: (fieldName, placeholders) => {
+      return `The ${fieldName} field must have at least ${placeholders.min} characters and ${placeholders.max} characters at most`;
+    },*!/
+    // message: '{_field_} and {_value_} and {_rule_}'
   });
+
+  extend('one_of', (value, values) => {
+    return values.indexOf(value) !== -1;
+  })*/
 </script>
 
 <style scoped>
